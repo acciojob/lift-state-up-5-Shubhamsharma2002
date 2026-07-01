ed946aa9a2733ed009b00a365c9032c6fb660336
Shubham Sharma <shubhamtech004@gmail.com>
2026-07-01 18:26:53 +0530

    Completed Lift State Up 5

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Login from "./Login";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      {isLoggedIn ? (
+        <h1>You are logged in</h1>
+      ) : (
+        <Login setIsLoggedIn={setIsLoggedIn} />
+      )}
+    </div>
+  );
+};
+
+export default App;
